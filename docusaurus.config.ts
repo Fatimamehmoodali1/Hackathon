560176487c265f160a6ad3cdace06b2ff1170c79
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-12-07 21:13:39 +0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,12 +45,7 @@ const config: Config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
+        
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
