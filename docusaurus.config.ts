1dca813f2998b3b6edae8e1a54971e96faa16584
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2025-12-07 21:02:50 +0500

    Update site title and navbar title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Teaching Physical AI & Humanoid Robotics Course',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -75,7 +74,7 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'MyBook',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
